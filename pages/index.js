8c42c0632d0d19ed4e67d76ec650de6a812372af
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-07-06 22:19:03 +0200

    Restore

--- pages/index.js
+++ pages/index.js
@@ -8,32 +8,9 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
-      <div className={styles.navbar-container}>
-            <nav class="bar justify-content-center">
-                <ul class="nav">
-                    <li><p class="my-auto p-3 nav-item poppins-font">ask.apuem</p></li>
-                    <li>
-                        <p class="my-auto p-3 nav-item">
-                            Questions about
-                            <select name="select" class="text-decoration-underline">
-                                <option value="value1">Value 1</option>
-                                <option value="value2" selected>Value 2</option>
-                                <option value="value3">Value 3</option>
-                            </select>
-                        </p>
-                    </li>
-                    <li><p class="my-auto p-3 nav-item">12p</p></li>
-                </ul>
-            </nav>
-        </div>
         <div className={styles.bar}>
             <h1>ask.apuem</h1>
         </div>
